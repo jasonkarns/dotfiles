2497be859265d16fedcdff2b30057c3927386766
Mike Busch <busch.michael@gmail.com>
2009-09-25 22:57:52 -0400

    added PS1 and color to command line

--- .bash/prompt.sh
+++ .bash/prompt.sh
@@ -1,4 +1,4 @@
 _Yellow="$(tput bold ; tput setaf 3)"
 _NC="$(tput sgr0)" # No Color
 
-# export PS1='\W${_Yellow}$(__git_ps1 " (%s)")${_NC}\$ '
+export PS1='\h:\W${_Yellow}$(__git_ps1 " (%s)")${_NC} \$ '
