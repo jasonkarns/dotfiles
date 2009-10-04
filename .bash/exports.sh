6a268339abb58ef5d866b4671554fe2f39bc0691
Mike Busch <busch.michael@gmail.com>
2009-10-04 16:16:51 -0400

    changed CDPATH to match local project directory structure

--- .bash/exports.sh
+++ .bash/exports.sh
@@ -1,3 +1,3 @@
-export PATH="/opt/local/lib/postgresql82/bin:/usr/local/bin:/opt/local/bin:/bin:/sbin:/usr/bin:/usr/sbin:/sw/bin:/opt/local/jruby/current/bin"
-export CDPATH=.:~:~/Projects:~/Projects/EdgeCase/clients:~/Projects/Personal
+export PATH="/usr/local/bin:/opt/local/bin:/bin:/sbin:/usr/bin:/usr/sbin:/sw/bin"
+export CDPATH=.:~:~/projects/training:~/projects/toobla:~/projects/personal
 export LSCOLORS=gxfxcxdxbxegedabagacad
