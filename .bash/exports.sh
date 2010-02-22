a9bc567040b42761ebaf387d7836fc35820d5473
Mike Busch <busch.michael@gmail.com>
2010-02-22 10:09:16 -0500

    fixed cdpath

--- .bash/exports.sh
+++ .bash/exports.sh
@@ -2,5 +2,5 @@ export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK
 export M2_HOME=/usr/local/apache-maven/apache-maven-2.2.1
 export M2=$M2_HOME/bin
 export PATH="$M2:$JAVA_HOME/bin:/opt/subversion/bin:/usr/local/bin:/opt/local/bin:/bin:/sbin:/usr/bin:/usr/sbin:/sw/bin"
-export CDPATH=.:~:~/projects/training:~/projects/toobla:~/projects/personal:~/projects/freelance:~/projects/katas:~/projects/opensource:~/projects.pillar
+export CDPATH=.:~:~/projects/training:~/projects/toobla:~/projects/personal:~/projects/freelance:~/projects/katas:~/projects/opensource:~/projects/pillar
 export LSCOLORS=gxfxcxdxbxegedabagacad
