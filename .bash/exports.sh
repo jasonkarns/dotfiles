0570e7bc533e54a850e6c35e5d99c1457cd1b8a0
Mike Busch <busch.michael@gmail.com>
2010-04-15 09:55:42 -0400

    added java path variables

--- .bash/exports.sh
+++ .bash/exports.sh
@@ -1,6 +1,6 @@
-export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home
+export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home
 export M2_HOME=/usr/local/apache-maven/apache-maven-2.2.1
 export M2=$M2_HOME/bin
-export PATH="$M2:$JAVA_HOME/bin:/opt/subversion/bin:/usr/local/bin:/opt/local/bin:/bin:/sbin:/usr/bin:/usr/sbin:/sw/bin"
+export PATH="$M2:$JAVA_HOME/bin:/opt/subversion/bin:/opt/local/bin:/sw/bin:$PATH"
 export CDPATH=.:~:~/projects/training:~/projects/toobla:~/projects/personal:~/projects/freelance:~/projects/katas:~/projects/opensource:~/projects/pillar
 export LSCOLORS=gxfxcxdxbxegedabagacad
