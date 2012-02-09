3a66ed892ad0c6b9caf5f06ae7da40e45a3acdc4
Jason Karns <jason@karns.name>
2012-02-09 11:05:43 -0500

    added usage of encrypted_password. also export HTTPS_PROXY along with HTTP_PROXY

--- .bash/proxy.sh
+++ .bash/proxy.sh
@@ -1,8 +1,10 @@
 if [ "$USERDOMAIN" == "NWIE" ]; then
   export USERNAME=`echo $LOGNAME | tr "A-Z" "a-z"`
   PROXY_PWD=<pass>
+#  PROXY_PWD=$(jruby -e "require 'encrypted_password';puts EncryptedPassword")
   PROXY_PROTOCOL=<protocol>
   PROXY_SERVER=<server>
   PROXY_PORT=<port>
   export HTTP_PROXY=$PROXY_PROTOCOL://$USERNAME:$PROXY_PWD@$PROXY_SERVER:$PROXY_PORT
+  export HTTPS_PROXY=$HTTP_PROXY
 fi
