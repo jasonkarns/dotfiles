5c5f9a6a4ae46a1b9b8226d1ff58429120b1f27c
Jason Karns <jason@karns.name>
2012-02-23 11:36:44 -0500

    fixed typo. added alias for history | grep

--- .bash/aliases.sh
+++ .bash/aliases.sh
@@ -3,3 +3,4 @@ alias dir='ls --color=auto --format=vertical'
 alias vdir='ls --color=auto --format=long'
 alias lsl='ls -l'                              # long list
 alias lsa='ls -A'                              # all but . and ..
+alias hgrep='history | grep'
