650a80d5371239440536ef809c9fed6a1cf69a8d
Jason Karns <jason@karns.name>
2012-06-26 23:28:51 -0400

    improve ssh (putty/pagaent) loading and handle 64 bit

--- .bash/ssh.sh
+++ .bash/ssh.sh
@@ -1,4 +1,13 @@
 # use putty
-PUTTY_HOME=/c/Program\ Files\ \(x86\)/PuTTY
-export GIT_SSH=$PUTTY_HOME/plink.exe
-"$PUTTY_HOME/pageant.exe" ~/.ssh/github.ppk ~/.ssh/unfuddle.ppk &
+
+if [ -d "/c/program files (x86)" ]; then
+  PUTTY_HOME="/c/program files (x86)/putty"
+else
+  PUTTY_HOME="/c/program files/putty"
+fi
+
+#export GIT_SSH=$PUTTY_HOME/plink.exe
+
+if [ -f *.ppk ]; then
+  "$PUTTY_HOME/pageant.exe" `ls ~/.ssh/*.ppk` &
+fi
