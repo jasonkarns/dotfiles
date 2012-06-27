8447e9a4caf84ad696545b7b04e9bbe57d68dae8
Jason Karns <jason@karns.name>
2012-06-26 22:44:13 -0400

    update proxy.sh

--- .bash/proxy.sh
+++ .bash/proxy.sh
@@ -1,10 +1,9 @@
 if [ "$USERDOMAIN" == "NWIE" ]; then
   export USERNAME=`echo $LOGNAME | tr "A-Z" "a-z"`
-  PROXY_PWD=<pass>
-#  PROXY_PWD=$(jruby -e "require 'rubygems'; require 'encrypted_password';puts EncryptedPassword")
-  PROXY_PROTOCOL=<protocol>
-  PROXY_SERVER=<server>
-  PROXY_PORT=<port>
+  PROXY_PWD=$(jruby -e "require 'rubygems'; require 'encrypted_password';puts EncryptedPassword")
+  PROXY_PROTOCOL={protocol}
+  PROXY_SERVER={server}
+  PROXY_PORT={port}
   export HTTP_PROXY=$PROXY_PROTOCOL://$USERNAME:$PROXY_PWD@$PROXY_SERVER:$PROXY_PORT
   export HTTPS_PROXY=$HTTP_PROXY
 fi
