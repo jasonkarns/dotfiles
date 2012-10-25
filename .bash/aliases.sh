079fb819f98452f9d5487517df4c702494a3edb9
Jason Karns <jason@karns.name>
2012-10-25 07:29:26 -0400

    local machine-specific shell aliases

--- .bash/aliases.sh
+++ .bash/aliases.sh
@@ -4,3 +4,5 @@ alias lsa='ls -lA'           # show hidden files (except . and ..)
 alias lsart='ls -lart'       # order by modified, latest last
 alias hrep='history | grep'  # search command history
 alias g='grep -I --ignore-case --recursive --exclude-dir=.git --exclude-dir=tmp'
+
+[[ -f ~/.bash/aliases.sh.local ]] && source ~/.bash/aliases.sh.local
