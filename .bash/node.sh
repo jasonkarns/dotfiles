b85c5ecef8842473f3bd018160cacfae4e41a750
Jason Karns <jason@karns.name>
2014-02-14 17:40:42 -0500

    alias npm-pristine (shrinkwrap/remove node_modules/reinstall)

--- .bash/node.sh
+++ .bash/node.sh
@@ -2,3 +2,6 @@ export PATH="$PATH:/usr/local/share/npm/bin"
 
 # grunt tab completion
 . <(grunt --completion=bash)
+
+# sort-of pristine reinstall for npm
+alias npm-pristine='[ -f $(npm prefix)/npm-shrinkwrap.json ] || npm shrinkwrap && rm -rf $(npm prefix)/node_modules && npm install'
