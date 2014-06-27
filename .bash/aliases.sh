371815157f669d6898a454efc2ad04a4a698503e
Jason Karns <jason@karns.name>
2014-06-27 14:37:36 -0400

    quick-update aliases for vim bundles and brew

--- .bash/aliases.sh
+++ .bash/aliases.sh
@@ -5,6 +5,9 @@ alias lsart='ls -lart'       # order by modified, latest last
 alias hrep='history | grep'  # search command history
 alias g='grep -I --ignore-case --recursive --exclude-dir=.git --exclude-dir=tmp'
 
+alias upvim='vim +PluginUpdate +qall'
+alias upbrew='brew update && brew doctor && brew outdated'
+
 alias gemini='ssh -R1337:localhost:22 jasonkarns'
 
 [[ -f ~/.bash/aliases.sh.local ]] && source ~/.bash/aliases.sh.local
