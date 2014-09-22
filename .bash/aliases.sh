379af05cec5bf0ef83a9aed55fadaf4075639dae
Jason Karns <jason@karns.name>
2014-09-22 09:42:15 -0400

    may as well auto-update brew. I never ignore updates, anyway

--- .bash/aliases.sh
+++ .bash/aliases.sh
@@ -6,7 +6,7 @@ alias hrep='history | grep'  # search command history
 alias g='grep -I --ignore-case --recursive --exclude-dir=.git --exclude-dir=tmp'
 
 alias upvim='vim +PluginUpdate +PluginClean +qall'
-alias upbrew='brew update && brew doctor && brew outdated'
+alias upbrew='brew update && brew doctor && brew outdated && brew upgrade && brew cleanup'
 
 alias gemini='ssh -R1337:localhost:22 jasonkarns'
 
