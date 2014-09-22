dd60552692a90091c31465ae37c57ad04e386952
Jason Karns <jason@karns.name>
2014-09-22 09:42:35 -0400

    safer npm-update:
    see https://gist.github.com/othiym23/4ac31155da23962afd0e

--- .bash/aliases.sh
+++ .bash/aliases.sh
@@ -7,6 +7,7 @@ alias g='grep -I --ignore-case --recursive --exclude-dir=.git --exclude-dir=tmp'
 
 alias upvim='vim +PluginUpdate +PluginClean +qall'
 alias upbrew='brew update && brew doctor && brew outdated && brew upgrade && brew cleanup'
+alias upnpm='for package in $(npm -g outdated --parseable --depth=0 | cut -d: -f2); do npm -g install "$package"; done'
 
 alias gemini='ssh -R1337:localhost:22 jasonkarns'
 
