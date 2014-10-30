c67c776eec05fbbc9c7a112df0deb370e18658a7
Jason Karns <jason@karns.name>
2014-10-30 15:20:04 -0400

    upgrade all the things

--- .bash/aliases.sh
+++ .bash/aliases.sh
@@ -8,6 +8,7 @@ alias g='grep -I --ignore-case --recursive --exclude-dir=.git --exclude-dir=tmp'
 alias upvim='vim +PluginUpdate +PluginClean +qall'
 alias upbrew='brew update && brew doctor && brew outdated && brew upgrade && brew cleanup'
 alias upnpm='for package in $(npm -g outdated --parseable --depth=0 | cut -d: -f2); do npm -g install "$package"; done'
+alias upall='upbrew && upnpm && upvim'
 
 alias gemini='ssh -R1337:localhost:22 jasonkarns'
 
