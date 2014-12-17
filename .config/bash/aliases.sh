5cd30d794efecb11efed0260c47e1489c7b9a6a4
Jason Karns <jason@karns.name>
2014-12-17 10:11:50 -0500

    alias to update rbenv and plugins

--- .config/bash/aliases.sh
+++ .config/bash/aliases.sh
@@ -5,9 +5,10 @@ alias lsart='ls -lart'       # order by modified, latest last
 alias hrep='history | grep'  # search command history
 alias g='grep -I --ignore-case --recursive --exclude-dir=.git --exclude-dir=tmp'
 
-alias upvim='vim +PluginUpdate +PluginClean +qall'
 alias upbrew='brew update && brew doctor && brew outdated && brew upgrade && brew cleanup'
 alias upnpm='for package in $(npm -g outdated --parseable --depth=0 | cut -d: -f2); do npm -g install "$package"; done'
-alias upall='upbrew && upnpm && upvim'
+alias uprbenv='rbenv update'
+alias upvim='vim +PluginUpdate +PluginClean +qall'
+alias upall='upbrew && upnpm && uprbenv && upvim'
 
 alias gemini='ssh -R1337:localhost:22 jasonkarns'
