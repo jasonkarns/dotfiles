008ccd1426c1befd0fe3dd5189d3ffe11cb2126f
Jason Karns <jason@karns.name>
2015-03-03 12:29:13 -0500

    hold off on rm alias for now

--- .config/bash/aliases.sh
+++ .config/bash/aliases.sh
@@ -4,7 +4,7 @@ alias lsa='ls -lA'           # show hidden files (except . and ..)
 alias lsart='ls -lart'       # order by modified, latest last
 alias hrep='history | grep'  # search command history
 alias g='grep -I --ignore-case --recursive --exclude-dir=.git --exclude-dir=tmp'
-alias rm='rm -I'
+# alias rm='rm -I'
 
 alias upbrew='brew update && brew doctor && brew outdated && brew upgrade && brew cleanup'
 alias upnpm='for package in $(npm -g outdated --parseable --depth=0 | cut -d: -f2); do npm -g install "$package"; done'
