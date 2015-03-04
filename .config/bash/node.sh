41563c47f7233270230da6ebbda7f9650886d2a2
Jason Karns <jason@karns.name>
2015-03-04 11:26:59 -0500

    node cleanup

--- .config/bash/node.sh
+++ .config/bash/node.sh
@@ -1,9 +1,19 @@
-export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/config
 
-# grunt tab completion
-. <(grunt --completion=bash 2>/dev/null)
+##############
+# npm        #
+##############
+
+export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/config
 
 # sort-of pristine reinstall for npm
 alias npm-pristine='[ -f $(npm prefix)/npm-shrinkwrap.json ] || npm shrinkwrap && rm -rf $(npm prefix)/node_modules && npm install'
 
 alias npmexec='PATH=$(npm bin):$PATH'
+
+
+##############
+# grunt      #
+##############
+
+# grunt tab completion
+. <(grunt --completion=bash 2>/dev/null)
