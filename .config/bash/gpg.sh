b45a2e7d1a09a83397f1117e46d4803cb04c9be3
Jason Karns <jason@karns.name>
2016-04-04 21:26:56 -0400

    keybase service needs GNUPGHOME set via launchctl

--- .config/bash/gpg.sh
+++ .config/bash/gpg.sh
@@ -1,10 +1,12 @@
 export GNUPGHOME=$XDG_CONFIG_HOME/gpg
 
+launchctl setenv GNUPGHOME "$GNUPGHOME"
+
 return # short circuit the gpg daemon stuff (MacGPG is handling it now)
 
 envfile="${GNUPGHOME}/.gpg-agent-info"
 
-if [ -f "$envfile" ] && kill -0 `cut -d: -f 2 "$envfile"` 2>/dev/null; then
+if [ -f "$envfile" ] && kill -0 "$( cut -d: -f 2 "$envfile" )" 2>/dev/null; then
   eval $(cat "$envfile")
 else
   eval $(gpg-agent --daemon --write-env-file "$envfile")
