857c9a8f4df6af35d0bb65bda1a6a34c9992dd49
Jason Karns <jason@karns.name>
2016-05-10 16:39:42 -0400

    remove unnecessary shell aliases

--- .config/bash/bashrc.d/git.bash
+++ .config/bash/bashrc.d/git.bash
@@ -3,33 +3,13 @@
 #########################
 
 alias gs='git status'
-alias ga='git add'
 alias gap='git add -p'
 alias grp='git reset -p'
 alias gd='git diff'
 alias gdword='git diff --color-words'
 alias gdc='git diff --cached'
-alias go='git checkout'
 alias gx='gitx --all'
-alias glog='git glog'
-alias gh='git hist'
-alias gfa='git fa'
-alias giton='export GIT_PS1_SHOWDIRTYSTATE=true && export GIT_PS1_SHOWSTASHSTATE=true && export GIT_PS1_SHOWUPSTREAM=verbose'
-alias gitoff='unset GIT_PS1_SHOWDIRTYSTATE && unset GIT_PS1_SHOWSTASHSTATE && unset GIT_PS1_SHOWUPSTREAM'
 
-
-#alias gbc='git branch --contains'
-#alias glp='git log -m -p -- '
-#alias glps='git log -p -S'
-#alias gbl='git blame'
-#alias gg='git grep'
-
-
-#########################
-# ps1
-#########################
-
-# see ~/.bash/ps1.sh
 
 #########################
 # contrib
