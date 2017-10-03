7aefe7b96031a78b7de9d8098fe2667d38b11c26
Jason Karns <jason@karns.name>
2017-10-02 21:14:39 -0400

    yml extension for bundler config

--- .config/bashrc.d/ruby.bash
+++ .config/bashrc.d/ruby.bash
@@ -19,7 +19,7 @@ export GEM_SPEC_CACHE=$XDG_CACHE_HOME/gem/specs
 #########################
 
 # https://github.com/bundler/bundler/pull/6024
-export BUNDLE_USER_CACHE=$XDG_CACHE_HOME/bundler
+export BUNDLE_USER_CACHE=$XDG_CACHE_HOME/bundler.yml
 export BUNDLE_USER_CONFIG=$XDG_CONFIG_HOME/bundler
 export BUNDLE_USER_PLUGIN=$XDG_DATA_HOME/bundler
 
