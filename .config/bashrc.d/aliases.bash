eac4e31fe0160e767efc38d606e9d4ae27efc6a2
Jason Karns <jason@karns.name>
2017-10-02 21:07:47 -0400

    a failed brew update (typically due to unavailable tap) shouldn't kill the updating

--- .config/bashrc.d/aliases.bash
+++ .config/bashrc.d/aliases.bash
@@ -5,7 +5,7 @@ alias lsart='ls -lart'       # order by modified, latest last
 alias hrep='history | grep'  # search command history
 alias g='grep -I --ignore-case --recursive --exclude-dir=.git --exclude-dir=tmp'
 
-alias upbrew='brew update && brew doctor && brew outdated && brew upgrade && brew cleanup'
+alias upbrew='brew update ; brew doctor && brew outdated && brew upgrade && brew cleanup'
 alias upnpm='nodenv each npm update -g --quiet'
 alias upnodenv='nodenv update && nodenv update-version-defs --dry-run'
 alias uprbenv='rbenv update && rbenv update-version-defs --dry-run'
