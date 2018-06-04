2433fdb978bc1e2e8c631e3850cef5313f45beae
Jason Karns <jason@karns.name>
2018-06-04 11:34:19 -0400

    remove shutterstock configs

--- .config/bashrc.d/git.bash
+++ .config/bashrc.d/git.bash
@@ -10,20 +10,6 @@ alias gdword='git diff --color-words'
 alias gdc='git diff --cached'
 alias gx='gitx --all'
 
-alias hubs='GITHUB_HOST=github.shuttercorp.net hub'
-alias gists='GITHUB_URL=https://github.shuttercorp.net gist'
-
-# disable DWIM guessing for branch name completion for checkout
-__git_completion_checkout_no_guess() {
-  if [[ $PWD/ =~ /shutterstock/base-web/ ]]; then
-    export GIT_COMPLETION_CHECKOUT_NO_GUESS=1
-  else
-    unset GIT_COMPLETION_CHECKOUT_NO_GUESS
-  fi
-}
-
-after_cd __git_completion_checkout_no_guess
-
 #########################
 # contrib
 #########################
--- .config/vim/after/plugin/fugitive.vim
+++
@@ -1,1 +0,0 @@
-let g:fugitive_github_domains = [ 'https://github.shuttercorp.net' ]
