04fffea6dac95ffd23e8247c7a950e7aab5644a1
Jason Karns <jason@karns.name>
2019-02-24 18:12:49 -0500

    Fix shell history
    Re-tweak the built-in /etc/bashrc_Apple_Terminal
    
    Move history config to profile init phase.
    Also, HIST* vars don't need exported.

--- .config/bashrc.d/history.bash
+++
@@ -1,6 +0,0 @@
-export HISTCONTROL=ignoredups:erasedups:ignorespace
-export HISTSIZE=10000
-export HISTIGNORE="fg*"
-export HISTFILE=$XDG_DATA_HOME/bash/history
-
-# see config.bash.mac for apple-specific settings
---
+++ .config/bashrc.d/history.sh
@@ -0,0 +1,5 @@
+# see config.bash.mac for apple-specific settings
+HISTCONTROL=ignoredups:erasedups:ignorespace
+HISTSIZE=10000
+HISTIGNORE="fg*"
+HISTFILE=$XDG_DATA_HOME/bash/history
