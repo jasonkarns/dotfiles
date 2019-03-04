6bd8cb45d5a5b6c62fe761790b681f713d21bbf7
Jason Karns <jason@karns.name>
2019-03-03 22:46:01 -0500

    Ensure homebrew git config applies to homebrew cache clones

--- .config/bashrc.d/brew.sh
+++ .config/bashrc.d/brew.sh
@@ -2,7 +2,9 @@
 eval "$(brew shellenv)"
 
 # remove the redundant prepended paths
-PATH="${PATH#/usr/local/bin:/usr/local/sbin:}"
+PATH=${PATH#/usr/local/bin:/usr/local/sbin:}
 
 # disable analytics
 export HOMEBREW_NO_ANALYTICS=1
+
+export HOMEBREW_CACHE=$XDG_CACHE_HOME/Homebrew
