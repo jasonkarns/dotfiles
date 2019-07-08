b9fb6fa9e82063c8188fda382ca65be1934548c2
Jason Karns <jason@karns.name>
2019-07-08 09:52:35 -0400

    Latest homebrew supports skipping formula
    Formula skipping is done via env-var instead of yaml file.

--- .config/bashrc.d/client-github.bash
+++ .config/bashrc.d/client-github.bash
@@ -1,4 +1,9 @@
+# shellcheck disable=1004,2016
+
 export GIT_COMPLETION_CHECKOUT_NO_GUESS=0
 
-# shellcheck disable=2016
 __chwd_hook '[[ "$PWD" != "$HOME/Projects/github/github"* ]]; GIT_COMPLETION_CHECKOUT_NO_GUESS=$?'
+__chwd_hook 'if [[ "$PWD" == "$HOME/Projects/github/github"* ]]; \
+              then HOMEBREW_BUNDLE_BREW_SKIP="awssume docker docker-machine"; \
+              else unset HOMEBREW_BUNDLE_BREW_SKIP; \
+            fi'
