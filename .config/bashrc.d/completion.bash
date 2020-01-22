940f3a0874c09c3f2e32cb163f2254752d29f5ed
Jason Karns <jason.karns@gmail.com>
2020-01-22 10:56:23 -0500

    fix bash-completion after 2.10 update

--- .config/bashrc.d/completion.bash
+++ .config/bashrc.d/completion.bash
@@ -1,4 +1,5 @@
 # shellcheck disable=SC1090
 
 # enable shell completion
-test -r "$HOMEBREW_PREFIX/share/bash-completion/bash_completion" && . "$_"
+export BASH_COMPLETION_COMPAT_DIR="${HOMEBREW_PREFIX:?}/etc/bash_completion.d"
+test -r "${HOMEBREW_PREFIX:?}/etc/profile.d/bash_completion.sh" && . "$_"
