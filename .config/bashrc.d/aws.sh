553261c6f8a2bc63d74eafe93197253b82ae35b7
Jason Karns <jason.karns@gmail.com>
2020-03-11 09:05:54 -0400

    Fix aws xdg config
    credentials should be in data-home not config.

--- .config/bashrc.d/aws.sh
+++ .config/bashrc.d/aws.sh
@@ -1,2 +1,4 @@
-export AWS_SHARED_CREDENTIALS_FILE=$XDG_CONFIG_HOME/aws/credentials
-export AWS_CONFIG_FILE=$XDG_CONFIG_HOME/aws/config
+export AWS_CONFIG_FILE="$XDG_CONFIG_HOME/aws/config"
+export AWS_CLI_HISTORY_FILE="$XDG_DATA_HOME/aws/history"
+export AWS_CREDENTIALS_FILE="$XDG_DATA_HOME/aws/credentials"
+export AWS_SHARED_CREDENTIALS_FILE="$XDG_DATA_HOME/aws/shared-credentials"
