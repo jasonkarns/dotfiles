de459a749edbcd2c5e911cbe010c5e6e8370b33b
Jason Karns <jason.karns@gmail.com>
2020-03-15 17:57:51 -0400

    Set HOMEBREW_PREFIX manually to avoid the perf-hit of running brew

--- .config/bashrc.d/brew.sh
+++ .config/bashrc.d/brew.sh
@@ -1,10 +1,8 @@
-# setup paths for homebrew
-eval "$(brew shellenv)"
-
-# remove the redundant prepended paths
-PATH=${PATH#/usr/local/bin:/usr/local/sbin:}
+# set this manually; it's faster than: eval "$(brew shellenv)"
+# this is the default, but other scripts rely on them to avoid $(brew --prefix)
+export HOMEBREW_PREFIX=/usr/local
 
 # disable analytics
 export HOMEBREW_NO_ANALYTICS=1
 
-export HOMEBREW_CACHE=$XDG_CACHE_HOME/Homebrew
+export HOMEBREW_CACHE="$XDG_CACHE_HOME/Homebrew"
