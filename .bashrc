d0413cfff251622e183b901ea20ac88a51ff526e
Jason Karns <jason.karns@gmail.com>
2020-04-29 10:12:11 -0400

    __chwd_hook now does double duty for setting and evaling chwd hooks

--- .bashrc
+++ .bashrc
@@ -5,6 +5,3 @@ for config in "$XDG_CONFIG_HOME"/bashrc.d/*.bash{,.local}; do
   # shellcheck disable=SC1090
   test -r "$config" && . "$_"
 done
-
-# all bashrc.d configs are sourced now, we can unset helpers
-unset __chwd_hook
--- .config/bashrc.d/chwd.bash
+++ .config/bashrc.d/chwd.bash
@@ -6,19 +6,22 @@
 __CHWD_HOOKS=""
 
 # The prototype function which will be defined per builtin
-__chwd__() {
-  builtin __chwd__ "$@" || return
-  eval $__CHWD_HOOKS || true
+__chwd_proto__() {
+  builtin __chwd_proto__ "$@" || return
+  __chwd_hook --eval
 }
 
 for c in cd popd pushd; do
   # print the prototype function, substituting the wrapped builtin
   # and eval the output to define the new wrapper function
-  eval "$(declare -f __chwd__ | sed "s/__chwd__/$c/")"
-done && unset __chwd__
+  eval "$(declare -f __chwd_proto__ | sed "s/__chwd_proto__/$c/")"
+done && unset __chwd_proto__
 
 __chwd_hook() {
-  __CHWD_HOOKS+="${1%;}; "
+  case "$1" in
+  --eval) eval "$__CHWD_HOOKS" || true;;
+  *) __CHWD_HOOKS+="${1%;}; "
+  esac
 }
 
 __chwd_entering() {
