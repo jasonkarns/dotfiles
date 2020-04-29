63155a0e4b9437149ecda3d6b6d0df31434ffef4
Jason Karns <jason.karns@gmail.com>
2020-04-29 10:58:03 -0400

    More expansive homebrew skiplist

--- .config/bashrc.d/brew.sh
+++ .config/bashrc.d/brew.sh
@@ -8,4 +8,5 @@ export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_CACHE="$XDG_CACHE_HOME/Homebrew"
 
 # don't install the following formula when running brew-bundle
-export HOMEBREW_BUNDLE_BREW_SKIP="awssume docker docker-machine"
+export HOMEBREW_BUNDLE_BREW_SKIP="rakudo-star mkcert nss aws-iam-authenticator docker docker-machine awscli awssume imagemagick kubectl@1.14 kustomize@2.0 container-diff"
+export HOMEBREW_BUNDLE_CASK_SKIP="github/bootstrap/zulu8"
