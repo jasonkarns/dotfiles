b63cc58c9fa6cca3ad55c5869208e0c44a9dd9e9
Jason Karns <jason.karns@gmail.com>
2022-11-21 11:57:26 -0500

    Configured homebrew

--- .config/bashrc.d/brew.sh
+++ .config/bashrc.d/brew.sh
@@ -7,6 +7,16 @@ export HOMEBREW_NO_ANALYTICS=1
 
 export HOMEBREW_CACHE="$XDG_CACHE_HOME/Homebrew"
 
-# don't install the following formula when running brew-bundle
+# wire homebrew's paths into standard tooling; see `brew shellenv`
+export PATH="${HOMEBREW_PREFIX:?}/bin:${HOMEBREW_PREFIX:?}/sbin${PATH+:$PATH}"
+export HOMEBREW_CELLAR="${HOMEBREW_PREFIX:?}/Cellar"
+export HOMEBREW_REPOSITORY="${HOMEBREW_PREFIX:?}"
+export MANPATH="${HOMEBREW_PREFIX:?}/share/man${MANPATH+:$MANPATH}:"
+export INFOPATH="${HOMEBREW_PREFIX:?}/share/info:${INFOPATH:-}"
+
+# explicitly skip formulae from Brewfiles; space-separated
 # export HOMEBREW_BUNDLE_BREW_SKIP=
 # export HOMEBREW_BUNDLE_CASK_SKIP=
+# export HOMEBREW_BUNDLE_MAS_SKIP=
+# export HOMEBREW_BUNDLE_WHALEBREW_SKIP=
+# export HOMEBREW_BUNDLE_TAP_SKIP=
