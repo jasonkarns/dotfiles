13e7d515d5637d213302eaf2f3b0fe4a39c41a3a
Jason Karns <jason.karns@gmail.com>
2022-11-21 11:46:00 -0500

    fixup! M1 homebrew location

--- .config/bashrc.d/brew.sh
+++ .config/bashrc.d/brew.sh
@@ -1,6 +1,6 @@
 # set this manually; it's faster than: eval "$(brew shellenv)"
 # this is the default, but other scripts rely on them to avoid $(brew --prefix)
-export HOMEBREW_PREFIX=/opt/homebrew;
+export HOMEBREW_PREFIX=/opt/homebrew # (/usr/local on intel arch)
 
 # disable analytics
 export HOMEBREW_NO_ANALYTICS=1
@@ -8,5 +8,5 @@ export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_CACHE="$XDG_CACHE_HOME/Homebrew"
 
 # don't install the following formula when running brew-bundle
-export HOMEBREW_BUNDLE_BREW_SKIP="rakudo-star mkcert nss aws-iam-authenticator docker docker-machine awscli awssume imagemagick kubectl@1.14 kustomize@2.0 container-diff"
-export HOMEBREW_BUNDLE_CASK_SKIP="github/bootstrap/zulu8"
+# export HOMEBREW_BUNDLE_BREW_SKIP=
+# export HOMEBREW_BUNDLE_CASK_SKIP=
