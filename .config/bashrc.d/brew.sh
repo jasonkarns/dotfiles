c7d32cef0fa5df2c74aefde18ee3238c39376270
Jason Karns <jason.karns@gmail.com>
2024-02-09 11:14:01 -0500

    brew setup

--- .config/bashrc.d/brew.sh
+++ .config/bashrc.d/brew.sh
@@ -1,18 +1,19 @@
-# set this manually; it's faster than: eval "$(brew shellenv)"
-# this is the default, but other scripts rely on them to avoid $(brew --prefix)
-export HOMEBREW_PREFIX=/opt/homebrew # (/usr/local on intel arch)
+# set this explicitly; it's faster than: eval "$(brew shellenv)"
+# this is the default, but other scripts rely on them to avoid overhead of $(brew --prefix)
+# /opt/homebrew on apple silicon; /usr/local on intel
+export HOMEBREW_PREFIX=/opt/homebrew
+
+# wire homebrew's paths into standard tooling; see `brew shellenv`
+export HOMEBREW_CELLAR="${HOMEBREW_PREFIX:?}/Cellar"
+export HOMEBREW_REPOSITORY="${HOMEBREW_PREFIX:?}"
+export PATH="${HOMEBREW_PREFIX:?}/bin:${HOMEBREW_PREFIX:?}/sbin${PATH+:$PATH}"
+export MANPATH="${HOMEBREW_PREFIX:?}/share/man${MANPATH+:$MANPATH}:"
+export INFOPATH="${HOMEBREW_PREFIX:?}/share/info:${INFOPATH:-}"
 
 # disable analytics
 export HOMEBREW_NO_ANALYTICS=1
 
 export HOMEBREW_CACHE="$XDG_CACHE_HOME/Homebrew"
-
-# wire homebrew's paths into standard tooling; see `brew shellenv`
-export PATH="${HOMEBREW_PREFIX:?}/bin:${HOMEBREW_PREFIX:?}/sbin${PATH+:$PATH}"
-export HOMEBREW_CELLAR="${HOMEBREW_PREFIX:?}/Cellar"
-export HOMEBREW_REPOSITORY="${HOMEBREW_PREFIX:?}"
-export MANPATH="${HOMEBREW_PREFIX:?}/share/man${MANPATH+:$MANPATH}:"
-export INFOPATH="${HOMEBREW_PREFIX:?}/share/info:${INFOPATH:-}"
 
 # explicitly skip formulae from Brewfiles; space-separated
 # set the vars so they can be appended elsewhere
